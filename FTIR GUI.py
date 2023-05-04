42dea68618af66f0f3ff381418b620030f4945fd
Ravindu-Kalhara <>
2023-05-04 09:36:10 +0530

    feat: add new fuctionalities
    In this commit some new features are added
    - Instead of adding manually FTIR data file names into textbox, now by
    using tk framework based filepicker user can easily select FTIR data
    file which he wants to plot (Multilple selection are also alowed).
    - Changes the layout of tk window from "pack" to "grid".
    - Remove unwanted buttons.
    - Changes some comments and essecntial texts.

--- FTIR GUI.py
+++ FTIR GUI.py
@@ -1,59 +1,66 @@
+import os
 import tkinter as tk
 import pandas as pd
 import matplotlib.pyplot as plt
+from tkinter import filedialog
+from Essentials import Essentials
 
 # Create the main window
 window = tk.Tk()
+window.title("FTIR DATA PLOTER")
+filepaths_str = tk.StringVar()
 
-# Set the window title
-window.title("CSV PLOTER")
+Ess = Essentials()
 
-# Create the label
-label = tk.Label(window, text="Enter your filenames here (comma-separated):")
+def open_file_dialog(filenames_txtbox: tk.Text, filepaths_str: tk.StringVar) -> None:
+    filepaths = filedialog.askopenfilenames(initialdir=".", title="Select files",
+                                            filetypes=[("Text files", "*.csv")], multiple=True)  # type: ignore
 
-# Create the textbox
-textbox = tk.Text(window, height=1, width=30)
+    filepaths_str.set(",".join(filepaths))  # save the path of all selected data files.
+    # format data file names to display in text box
+    txtbox_str = ",".join(map(lambda x: x.split(os.sep)[-1], filepaths))
 
-# Create the buttons
+    filenames_txtbox.configure(state=tk.NORMAL)
+    filenames_txtbox.delete('1.0', tk.END)
+    filenames_txtbox.insert(tk.END, txtbox_str)
+    filenames_txtbox.configure(state=tk.DISABLED)
 
 
-def run_code():
+def run_code(filepaths_str: tk.StringVar, filenames_txtbox: tk.Text) -> None:
     # Get the filenames from the textbox
-    filenames = textbox.get("1.0", "end").strip().split(",")
+    file_paths = filepaths_str.get().split(",")
 
     # Load the csv files into pandas dataframes
-    dataframes = [pd.read_csv(filename.strip() + ".csv", skiprows=1) for filename in filenames]
+    dataframes = [pd.read_csv(filename, skiprows=1) for filename in file_paths]
 
-    # Create a plot of the "cm-1" column of the first dataframe
+    # Defining plots
     fig, ax = plt.subplots()
-    dataframes[0].plot('cm-1', ax=ax)
-
-    # Add the "cm-1" column of the other dataframes to the plot
-    for dataframe in dataframes[1:]:
-        dataframe.plot("cm-1", ax=ax)
+    for dataframe in dataframes:
+        dataframe.plot("cm-1", ax=ax)  # type: ignore
 
     # Set the axis labels and title
     ax.set_xlabel("cm-1")
     ax.set_ylabel("%T")
-    ax.set_title("CSV Files Combined Plot")
-
-    # Add a legend
+    ax.set_title("Transmition vs wave number")
     ax.legend()
-
-    # Display the plot
     plt.show()
 
+    # clear the text box containing data file names.
+    filenames_txtbox.configure(state=tk.NORMAL)
+    filenames_txtbox.delete('1.0', tk.END)
+    filenames_txtbox.configure(state=tk.DISABLED)
 
-button1 = tk.Button(window, text="Run Code", command=run_code)
-button2 = tk.Button(window, text="Clear", command=lambda: textbox.delete('1.0', 'end'))
+
+# defining the elements in the window
+textbox = tk.Text(window, height=1, width=30, state="disabled")
+btn1 = tk.Button(window, text="Select Data file(s)", command=lambda: open_file_dialog(textbox, filepaths_str))
+button1 = tk.Button(window, text="Display Graph(s)", command=lambda: run_code(filepaths_str, textbox))
 button3 = tk.Button(window, text="Close", command=window.destroy)
 
 # Pack the label, textbox, and buttons in the window
-label.pack()
-textbox.pack()
-button1.pack()
-button2.pack()
-button3.pack()
+textbox.grid(row=0, column=0, columnspan=3)
+btn1.grid(row=1, column=0)
+button1.grid(row=1, column=1)
+button3.grid(row=1, column=2)
 
-# Start the main loop
 window.mainloop()
